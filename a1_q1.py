9f4d2c343f00dabad8e62731e72a70652b0ccf10
CompAgronUser <101959973+CompAgronUser@users.noreply.github.com>
2023-05-30 14:54:19 +0800

    Update a1_q1.py

--- a1_q1.py
+++ a1_q1.py
@@ -3,6 +3,6 @@ import math
 
 def a1_q1(temperature, degree):
 
-    answer = (temperature - 32) * 5 / 9  # F to C
-    # answer =  temperature * 9 / 5 + 32  # C to F
+    # answer = (temperature - 32) * 5 / 9  # F to C
+    answer =  temperature * 9 / 5 + 32  # C to F
     return answer
