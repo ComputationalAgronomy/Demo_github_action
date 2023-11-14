9b1534e3139cefe9188b9bfb0b282c43237c42cf
CompAgronUser <101959973+CompAgronUser@users.noreply.github.com>
2023-11-14 16:05:56 +0800

    Update a1_q1.py

--- a1_q1.py
+++ a1_q1.py
@@ -2,7 +2,11 @@ import math
 
 
 def a1_q1(temperature, degree):
-
+    try:
+        raise TypeError('bad type')
+    except Exception as e:
+        e.add_note('Add some information')
+        raise
     # answer = (temperature - 32) * 5 / 9  # F to C
     answer =  temperature * 9 / 5 + 32  # C to F
     return answer
